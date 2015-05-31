76c45b0e65ac13e6b3c64bad78f3d13657198b4d
knyaz <petuhov_k@mail.ru>
2015-05-31 22:34:25 +0600

    missed files

---
+++ WebBrowser/Dom/TagsNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBrowser.Dom
+{
+	public static class TagsNames
+	{
+		public static string Script = "script";
+	}
+}
