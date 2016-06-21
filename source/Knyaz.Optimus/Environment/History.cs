2699ee86b0554c5e3434930522122ee3b6e3ca68
knyaz <petuhov_k@mail.ru>
2016-06-21 08:49:06 +0600

    extended history API by sketch

--- source/Knyaz.Optimus/Environment/History.cs
+++ source/Knyaz.Optimus/Environment/History.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Knyaz.Optimus.ScriptExecuting;
 using Knyaz.Optimus.Tools;
 
@@ -6,6 +7,17 @@ namespace Knyaz.Optimus.Environment
 {
 	public class History : IHistory
 	{
+		//todo: history should updated on navigatin using links, and Open method call.
+		readonly List<HistoryItem> _history = new List<HistoryItem>();
+		private int _currentPosition = 0;
+
+		class HistoryItem
+		{
+			public string Url;
+			public string Title;
+			public object Data;
+		}
+
 		private readonly Engine _engine;
 
 		public History(Engine engine)
@@ -13,10 +25,33 @@ namespace Knyaz.Optimus.Environment
 			_engine = engine;
 		}
 
-		public void PushState(object a, object b, string url)
+		public long Lenght { get { return _history.Count; } }
+
+		public void Go(long delta = 0)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Back()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Forward()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ReplaceState(object data, string title, string url)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PushState(object data, string title, string url)
 		{
 			//todo: raise events
-
+			_history.Add(new HistoryItem{Data = data, Title = title, Url = url});
+			_currentPosition++;
 			_engine.Uri = UriHelper.IsAbsolete(url) ? new Uri(url) : new Uri(new Uri(_engine.Uri.GetLeftPart(UriPartial.Authority)), url);
 		}
 	}
@@ -24,6 +59,11 @@ namespace Knyaz.Optimus.Environment
 	[DomItem]
 	public interface IHistory
 	{
-		void PushState(object a, object b, string url);
+		long Lenght { get; }
+		void Go(long delta = 0);
+		void Back();
+		void Forward();
+		void ReplaceState(object data, string title, string url);
+		void PushState(object data, string title, string url);
 	}
 }
