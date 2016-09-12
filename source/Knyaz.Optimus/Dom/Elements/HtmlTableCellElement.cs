73f1bd5c7fae409730b635e391d55c778e7848bc
knyaz <petuhov_k@mail.ru>
2016-09-12 18:58:32 +0700

    #1197. added missed files.

---
+++ source/Knyaz.Optimus/Dom/Elements/HtmlTableCellElement.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Knyaz.Optimus.Dom.Elements
+{
+	/// <summary>
+	/// https://www.w3.org/2003/01/dom2-javadoc/org/w3c/dom/html2/HTMLTableCellElement.html
+	/// </summary>
+	public class HtmlTableCellElement : HtmlElement
+	{
+		public HtmlTableCellElement(Document ownerDocument) : base(ownerDocument, TagsNames.Td)
+		{
+		}
+
+		/// <summary>
+		/// Abbreviation for header cells.
+		/// </summary>
+		public string Abbr
+		{
+			get { throw new NotImplementedException();}
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Horizontal alignment of data in cell.
+		/// </summary>
+		/// <returns></returns>
+		public string Align
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Names group of related headers.
+		/// </summary>
+		/// <returns></returns>
+		public string Axis
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Cell background color.
+		/// </summary>
+		/// <returns></returns>
+		public string BgColor
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// The index of this cell in the row, starting from 0.
+		/// </summary>
+		/// <returns></returns>
+		public int CellIndex
+		{
+			get { throw new NotImplementedException();}
+		}
+
+		/// <summary>
+		/// Alignment character for cells in a column.
+		/// </summary>
+		/// <returns></returns>
+		public string Ch
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Offset of alignment character.
+		/// </summary>
+		/// <returns></returns>
+		public string ChOff
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Number of columns spanned by cell.
+		/// </summary>
+		/// <returns></returns>
+		public int ColSpan
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// List of id attribute values for header cells.
+		/// </summary>
+		/// <returns></returns>
+		public string Headers
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Cell height.
+		/// </summary>
+		/// <returns></returns>
+		public string Height
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Suppress word wrapping.
+		/// </summary>
+		/// <returns></returns>
+		public bool NoWrap
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Number of rows spanned by cell.
+		/// </summary>
+		/// <returns></returns>
+		public int RowSpan
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Scope covered by header cells.
+		/// </summary>
+		/// <returns></returns>
+		public string Scope
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Vertical alignment of data in cell.
+		/// </summary>
+		/// <returns></returns>
+		public string VAlign
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Cell width.
+		/// </summary>
+		/// <returns></returns>
+		public string Width
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+	}
+}
---
+++ source/Knyaz.Optimus/Dom/Elements/HtmlTableRowElement.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+namespace Knyaz.Optimus.Dom.Elements
+{
+	/// <summary>
+	/// https://www.w3.org/2003/01/dom2-javadoc/org/w3c/dom/html2/HTMLTableRowElement.html
+	/// </summary>
+	public class HtmlTableRowElement : HtmlElement
+	{
+		public HtmlTableRowElement(Document ownerDocument) : base(ownerDocument, TagsNames.Tr)
+		{
+			Cells = new HtmlCollection(() => ChildNodes.OfType<HtmlTableCellElement>());
+		}
+
+		/// <summary>
+		/// Delete a cell from the current row.
+		/// </summary>
+		/// <param name="index"></param>
+		public void DeleteCell(int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Horizontal alignment of data within cells of this row.
+		/// </summary>
+		public string Align
+		{
+			get { throw new NotImplementedException();}
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Background color for rows.
+		/// </summary>
+		public string BgColor
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// The collection of cells in this row.
+		/// </summary>
+		public HtmlCollection Cells { get; private set; }
+
+		/// <summary>
+		/// Alignment character for cells in a column.
+		/// </summary>
+		public string Ch
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Offset of alignment character.
+		/// </summary>
+		public string ChOff
+		{
+			get { throw new NotImplementedException(); }
+			set { throw new NotImplementedException(); }
+		}
+		
+		/// <summary>
+		/// This is in logical order and not in document order.
+		/// </summary>
+		public int RowIndex
+		{
+			get { throw new NotImplementedException();}
+		}
+
+		/// <summary>
+		/// The index of this row, relative to the current section ( THEAD, TFOOT, or TBODY), starting from 0.
+		/// </summary>
+		public int SectionRowIndex
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		/// <summary>
+		/// Vertical alignment of data within cells of this row.
+		/// </summary>
+		public string VAlign
+		{
+			get { throw new NotImplementedException();}
+			set { throw new NotImplementedException(); }
+		}
+
+
+		/// <summary>
+		/// Insert an empty TD cell into this row.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns></returns>
+		public HtmlElement InsertCell(int index)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
