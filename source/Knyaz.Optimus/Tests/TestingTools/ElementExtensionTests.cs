5158d158a38d98e429d5fc3a143a82c7c35d34f0
knyaz <petuhov_k@mail.ru>
2017-05-08 08:46:50 +0700

    added tests.

--- source/Knyaz.Optimus/Tests/TestingTools/ElementExtensionTests.cs
+++ source/Knyaz.Optimus/Tests/TestingTools/ElementExtensionTests.cs
@@ -10,7 +10,9 @@ namespace Knyaz.Optimus.Tests.TestingTools
 	public class ElementExtensionTests
 	{
 		[TestCase("<table><tbody><tr id=row_0><td></td></tr><tr id=row_1><td></td></tr></tbody></table>", "tr[id^='row_']", "row_0,row_1")]
+		[TestCase("<table><tbody><tr id=row_0><td></td></tr><tr id=row_1><td></td></tr></tbody></table>", "tr[id^=row_]", "row_0,row_1")]
 		[TestCase("<tr id=row_0><td column-name=Name id=c1></td><td  columne-name=Id id=c2></td></tr>", "[column-name='Name']", "c1")]
+		[TestCase("<tr id=row_0><td column-name=Name id=c1></td><td  columne-name=Id id=c2></td></tr>", "[column-name=Name]", "c1")]
 		public void Select(string html, string selector, string expectedIds)
 		{
 			var doc = new Document();
