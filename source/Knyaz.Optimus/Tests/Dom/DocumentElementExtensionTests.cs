730e4dd2512312a7f5c99a2effbf974efa697d9f
knyaz <petuhov_k@mail.ru>
2017-10-30 15:38:34 +0700

    added some missed test.

---
+++ source/Knyaz.Optimus/Tests/Dom/DocumentElementExtensionTests.cs
@@ -0,0 +1,23 @@
+﻿#if NUNIT
+using System.Linq;
+using Knyaz.Optimus.Dom;
+using Knyaz.Optimus.Dom.Elements;
+using NUnit.Framework;
+
+namespace Knyaz.Optimus.Tests.Dom
+{
+	[TestFixture]
+	public class DocumentElementExtensionTests
+	{
+		[Test]
+		public void FlattenTest()
+		{
+			var doc = new Document();
+			var div = doc.CreateElement("DIV");
+			div.InnerHTML = "<p><span></span></p><br/>";
+			var res = div.Flatten().Select(x => x.NodeName).ToArray();
+			Assert.AreEqual(new[]{"DIV","P","SPAN","BR"}, res);
+		}
+	}
+}
+#endif
