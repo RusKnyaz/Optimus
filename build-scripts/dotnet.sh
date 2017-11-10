a80b03181d24f795cb177e6c9b2981a6cbe07e4b
knyaz <petuhov_k@mail.ru>
2017-11-10 23:12:52 +0700

    #1316. added build script

---
+++ build-scripts/dotnet.sh
@@ -0,0 +1,7 @@
+version="2.0.$BUILD_NUMBER-dev";
+fileversion="2.0.$BUILD_NUMBER.1";
+echo $version
+#dotnet build source/Knyaz.Optimus.sln /p:Version=$version /p:FileVersion=$fileversion
+#dotnet test source/Knyaz.Optimus.sln
+#dotnet publish source/Knyaz.Optimus.sln /p:Version=$version
+
