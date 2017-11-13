5f8d0ef09ecaac89161e2220470ee18c72c0a2f9
knyaz <petuhov_k@mail.ru>
2017-11-13 14:25:01 +0700

    try to run dotnet under www-data user.

--- build-scripts/dotnet.sh
+++ build-scripts/dotnet.sh
@@ -1,5 +1,6 @@
 version="2.0.$BUILD_NUMBER-dev";
 fileversion="2.0.$BUILD_NUMBER.1";
+HOME=$PWD
 /usr/bin/dotnet build source/Knyaz.Optimus.sln /p:Version=$version /p:FileVersion=$fileversion
 /usr/bin/dotnet test source/Knyaz.Optimus.sln
 /usr/bin/dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version
