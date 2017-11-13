00299e350ce9da3fe257e368ada00d7cf12bf67a
knyaz <petuhov_k@mail.ru>
2017-11-13 09:08:20 +0700

    updated script.

--- build-scripts/dotnet.sh
+++ build-scripts/dotnet.sh
@@ -1,7 +1,7 @@
 version="2.0.$BUILD_NUMBER-dev";
 fileversion="2.0.$BUILD_NUMBER.1";
 echo $version
-dotnet build source/Knyaz.Optimus.sln /p:Version=$version /p:FileVersion=$fileversion
-dotnet test source/Knyaz.Optimus.sln
-dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version
+/user/bin/dotnet build source/Knyaz.Optimus.sln /p:Version=$version /p:FileVersion=$fileversion
+/user/bin/dotnet test source/Knyaz.Optimus.sln
+/user/bin/dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version
 
