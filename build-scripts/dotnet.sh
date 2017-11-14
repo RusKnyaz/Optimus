012ce6dff99edc6545eabdf9a4fa9fa8ce11b93e
knyaz <petuhov_k@mail.ru>
2017-11-14 09:48:51 +0700

    #1316. Fixed build of net45 targets.

--- build-scripts/dotnet.sh
+++ build-scripts/dotnet.sh
@@ -1,6 +1,6 @@
 version="2.0.$BUILD_NUMBER-dev";
 fileversion="2.0.$BUILD_NUMBER.1";
-HOME=$PWD
-/usr/bin/dotnet build source/Knyaz.Optimus.sln /p:Version=$version /p:FileVersion=$fileversion
-/usr/bin/dotnet test source/Knyaz.Optimus.sln
-/usr/bin/dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version
+/usr/bin/dotnet build source/Knyaz.Optimus.sln /p:FrameworkPathOverride=/usr/lib/mono/4.5/ /p:Version=$version /p:FileVersion=$fileversion -c Debug -v n
+/usr/bin/dotnet test source/Knyaz.Optimus.sln --no-build
+/usr/bin/dotnet build source/Knyaz.Optimus.sln /p:FrameworkPathOverride=/usr/lib/mono/4.5/ /p:Version=$version /p:FileVersion=$fileversion -c Release -v n
+/usr/bin/dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version --no-build
