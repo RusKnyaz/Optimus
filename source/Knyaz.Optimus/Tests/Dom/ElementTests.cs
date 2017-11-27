c3e532c4051912d5bfa4cc923d508be3b923cc0e
knyaz <petuhov_k@mail.ru>
2017-11-27 18:35:02 +0700

    #1356. added tests.

--- source/Knyaz.Optimus/Tests/Dom/ElementTests.cs
+++ source/Knyaz.Optimus/Tests/Dom/ElementTests.cs
@@ -60,6 +60,44 @@ namespace Knyaz.Optimus.Tests.Dom
 			Assert.AreEqual("a b c f", div.GetAttribute("class"));
 		}
 
+		[Test]
+		public void ClassListLive()
+		{
+			var document = new Document();
+			var div = document.CreateElement("DIV");
+			div.ClassName = "a b c";
+			var classList = div.ClassList;
+			Assert.IsTrue(classList.Contains("a"));
+			Assert.IsFalse(classList.Contains("d"));
+			//change class
+			div.ClassName = "d e f";
+			classList.Assert(x =>
+				x.Contains("a") == false &&
+				x.Contains("d") == true);
+		}
+
+		[Test]
+		public void Contains()
+		{
+			var doc = new Document();
+			doc.Write("<div id=d1><div id=d2><div id=d3></div></div></div>");
+			var d1 = doc.GetElementById("d1");
+			var d2 = doc.GetElementById("d2");
+			var d3 = doc.GetElementById("d3");
+			Assert.IsTrue(d1.Contains(d2), "Contains child (d2)");
+			Assert.IsTrue(d1.Contains(d3), "Contains descendant (d3)");
+		}
+
+		[Test]
+		public void OwnerDocumentCanNotBeSetted()
+		{
+			var document = new Document();
+			var div = document.CreateElement("DIV");
+			var doc2 = new Document();
+			div.OwnerDocument = doc2;
+			Assert.AreNotEqual(doc2, div.OwnerDocument);
+		}
+
 		[TestCase("<div><div id=start name=target></div></div>", "div")]
 		[TestCase("<div name=target><span id=start></span></div>", "div")]
 		[TestCase("<div id=d1 name=target><div id=start></div></div>", "#d1")]
@@ -69,6 +107,44 @@ namespace Knyaz.Optimus.Tests.Dom
 			document.Write(html);
 			var start = document.GetElementById("start");
 			start.Closest(query).Assert(elt => elt.GetAttribute("name") == "target");
+		}
+
+		[TestCase("<div id=parent><dif id=ref></div><div id=new></div></div>", "<DIV id=\"new\"></DIV><DIV id=\"ref\"></DIV>", TestName = "InsertBefore_BothChildOfOne")]
+		[TestCase("<div id=parent><dif id=ref></div></div><div id=new></div>", "<DIV id=\"new\"></DIV><DIV id=\"ref\"></DIV>", TestName = "InsertBefore_DifferentParents")]
+		[TestCase("<div id=parent></div><div id=new></div>", "<DIV id=\"new\"></DIV>", TestName = "InsertBefore_ParentIsEmpty")]
+		[TestCase("<div id=parent><div>child</div></div><div id=new></div>", "<DIV>child</DIV><DIV id=\"new\"></DIV>", TestName = "InsertBefore_RefIsNull_AddsToEnd")]
+		public void InsertBefore(string sourceHtml, string resultHtml)
+		{
+			var doc = new Document();
+			doc.Write(sourceHtml);
+			var dnew = doc.GetElementById("new");
+			var dref = doc.GetElementById("ref");
+			var parent = doc.GetElementById("parent");
+			parent.InsertBefore(dnew, dref);
+			Assert.AreEqual(parent.InnerHTML, resultHtml);
+		}
+
+		[TestCase("<DIV id=parent><DIV id=ref></DIV><DIV id=new></DIV></DIV>", "<DIV id=\"parent\"><DIV id=\"new\"></DIV></DIV>")]
+		[TestCase("<DIV id=parent><DIV id=ref></DIV></DIV><DIV id=new></DIV>", "<DIV id=\"parent\"><DIV id=\"new\"></DIV></DIV>")]
+		public void ReplaceChild(string sourceHtml, string resultHtml)
+		{
+			var doc = new Document();
+			doc.Write(sourceHtml);
+			var dnew = doc.GetElementById("new");
+			var dref = doc.GetElementById("ref");
+			var parent = doc.GetElementById("parent");
+			parent.ReplaceChild(dnew, dref);
+			Assert.AreEqual(resultHtml, doc.Body.InnerHTML);
+		}
+
+		[Test]
+		public void ChildNodesLive()
+		{
+			var doc = new Document();
+			doc.Write("<div id=d1></div>");
+			var childNodes = doc.Body.ChildNodes;
+			doc.Body.InnerHTML = "<div></div><span></span>";
+			Assert.AreEqual(2, childNodes.Count);
 		}
 	}
 }
