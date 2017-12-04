5abad180b2adbc6c11dccbd103744637ea50f325
knyaz <petuhov_k@mail.ru>
2017-12-04 19:15:48 +0700

    added file

---
+++ source/Knyaz.Optimus.Tests/Resources/R.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+namespace Knyaz.Optimus.Tests.Resources
+{
+    class R
+    {
+		static string GetString(string file)
+		{
+			using (var reader = new StreamReader(typeof(R).Assembly.GetManifestResourceStream(file)))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		public static string JQueryJs => GetString("Knyaz.Optimus.Tests.Resources.jquery-2.1.3.js");
+		public static string KnockoutJs => GetString("Knyaz.Optimus.Tests.Resources.knockout.js");
+		public static string RequireJs => GetString("Knyaz.Optimus.Tests.Resources.requirejs.js");
+		public static string Template => GetString("Knyaz.Optimus.Tests.Resources.template.js");
+		public static string Text => GetString("Knyaz.Optimus.Tests.Resources.text.js");
+		public static string StringTemplateEngine => GetString("Knyaz.Optimus.Tests.Resources.stringTemplateEngine.js");
+		public static string JQueryFormJs => GetString("Knyaz.Optimus.Tests.Resources.jQuery.Form.js");
+	}
+}
