98cfeb784c950ae0bdcbf5f86a858a49ddcea963
knyaz <petuhov_k@mail.ru>
2017-12-21 19:44:04 +0700

    #1386. Fixed tests running. Suppresed warnings.

--- build-scripts/dotnet.sh
+++ build-scripts/dotnet.sh
@@ -9,7 +9,6 @@ else
 fi
 echo "Starting build of version: $version, fileVersion: $fileversion"
 echo "Step 1. Clean"
-/bin/rm -f source/Knyaz.Optimus/test-results/*
 /bin/rm -f source/Knyaz.Optimus.Tests/test-results/*
 /bin/rm -rf source/Knyaz.Optimus/bin/*
 /bin/rm -rf source/Knyaz.Optimus/obj/*
@@ -23,7 +22,7 @@ echo "Step 3. Build Knyaz.Optimus"
 echo "Step 4. Build Knyaz.Optimus.Tests"
 /usr/bin/dotnet build source/Knyaz.Optimus.Tests/Knyaz.Optimus.Tests.csproj /p:FrameworkPathOverride=/usr/lib/mono/4.5/ /p:Version=$version /p:FileVersion=$fileversion -c Release -v n
 echo "Step 5. Run tests"
-/usr/bin/dotnet test source/Knyaz.Optimus.Tests/Knyaz.Optimus.csproj --no-build /p:FrameworkPathOverride=/usr/lib/mono/4.5/ -f netcoreapp2.0 -r test-results --logger "trx;LogFileName=Optimus.netcore.trx"
+/usr/bin/dotnet test source/Knyaz.Optimus.Tests/Knyaz.Optimus.Tests.csproj --no-build /p:FrameworkPathOverride=/usr/lib/mono/4.5/ -f netcoreapp2.0 -r test-results --logger "trx;LogFileName=Optimus.netcore.trx"
 if [ "$versionDev" == "r" ]; then
 	echo "Step 6. Pack nupkg"
 	/usr/bin/dotnet pack source/Knyaz.Optimus/Knyaz.Optimus.csproj -c Release /p:Version=$version --no-build /p:FrameworkPathOverride=/usr/lib/mono/4.5/
