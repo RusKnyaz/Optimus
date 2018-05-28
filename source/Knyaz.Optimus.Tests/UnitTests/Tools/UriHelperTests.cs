eaf66421e72199285665f8063814cdecffe6883d
knyaz <petuhov_k@mail.ru>
2018-05-28 20:27:00 +0700

    #1411. Fixed test.

--- source/Knyaz.Optimus.Tests/UnitTests/Tools/UriHelperTests.cs
+++ source/Knyaz.Optimus.Tests/UnitTests/Tools/UriHelperTests.cs
@@ -10,6 +10,7 @@ namespace Knyaz.Optimus.Tests.UnitTests.Tools
 	{
 		[TestCase("data:text/html,<b>hi</b>", "text/html", "<b>hi</b>", TestName="PlainTextData")]
 		[TestCase("data:text/JavaScript;base64,Y29uc29sZS5sb2coJ2hpJyk7", "text/JavaScript", "console.log('hi');", TestName = "Base64Data")]
+		[TestCase("data:text/javascript;charset=utf8,window", "text/javascript", "window")]
 		public void Test(string uri, string expectedType, string expectedData)
 		{
 			var data = new Uri(uri).GetUriData();
