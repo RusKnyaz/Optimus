6fed1c36addd87cdacb29da0396b41b7dc7262ad
knyaz <petuhov_k@mail.ru>
2018-07-23 21:00:55 +0700

    #1430. Added missed files.

---
+++ source/Knyaz.Optimus.Tests/UnitTests/Dom/HtmlImageElementTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Knyaz.Optimus.Dom;
+using Knyaz.Optimus.Dom.Elements;
+using Knyaz.Optimus.Tools;
+using Moq;
+using NUnit.Framework;
+
+namespace Knyaz.Optimus.Tests.Dom
+{
+	[TestFixture]
+	public class HtmlImageElementTests
+	{
+		private Document _document;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_document = DomImplementation.Instance.CreateHtmlDocument();
+		}
+		
+		[Test]
+		public void ImageOnError()
+		{
+			_document.GetImage = s => Task.Run(() => (IImage)null);
+			
+			var img = (HtmlImageElement)_document.CreateElement(TagsNames.Img);
+			var errorSignal = new ManualResetEvent(false);
+			img.OnError += evt => { errorSignal.Set(); };
+
+			img.Src = "http://noimage.jpg";
+
+			Assert.IsTrue(errorSignal.WaitOne(1000));
+		}
+		
+		[Test]
+		public void ImageOnErrorTwice()
+		{
+			_document.GetImage = s => Task.Run(() => (IImage)null);
+			var img = (HtmlImageElement)_document.CreateElement(TagsNames.Img);
+			var errorSignal = new ManualResetEvent(false);
+			img.OnError += evt => { errorSignal.Set(); };
+
+			img.Src = "http://noimage.jpg";
+
+			Assert.IsTrue(errorSignal.WaitOne(1000), "first error");
+
+			errorSignal.Reset();
+			img.Src = "http://noimage.jpg";
+			
+			Assert.IsTrue(errorSignal.WaitOne(1000), "second error");
+		}
+
+		[Test]
+		public void ImageOnLoad()
+		{
+			var imageData = Mock.Of<IImage>(x => x.Width == 8 && x.Height == 4);
+			_document.GetImage = s => Task.Run(() => imageData);
+
+			var img = (HtmlImageElement)_document.CreateElement("img");
+			var loadSignal = new ManualResetEvent(false);
+			img.OnLoad += evt => { loadSignal.Set(); };
+
+			img.Src = "http://image.jpg";
+
+			Assert.IsTrue(loadSignal.WaitOne(1000));
+			Assert.AreEqual(8, img.NaturalWidth);
+			Assert.AreEqual(4, img.NaturalHeight);
+		}
+	}
+}
---
+++ source/Knyaz.Optimus/Dom/Elements/HtmlImageElement.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using Knyaz.Optimus.Dom.Events;
+using Knyaz.Optimus.Tools;
+
+namespace Knyaz.Optimus.Dom.Elements
+{
+	/// <summary>
+	/// The HTMLImageElement interface provides special properties and methods  for manipulating the layout and presentation of &lt;img&gt; elements.
+	/// Specs: https://www.w3.org/TR/2011/WD-html5-author-20110705/the-img-element.html#attr-img-ismap
+	/// </summary>
+	public class HtmlImageElement : HtmlElement
+	{
+		private readonly Func<string, Task<IImage>> _loadImage;
+		private readonly AttributeMappedValue<string> _src;
+		private readonly AttributeMappedValue<string> _alt;
+		private readonly AttributeMappedValue<string> _useMap;
+		private readonly AttributeMappedBoolValue _isMap;
+
+		private IImage _loadedImage = null;
+		
+		internal HtmlImageElement(Document ownerDocument, Func<string, Task<IImage>> loadImage) : base(ownerDocument, TagsNames.Img)
+		{
+			_loadImage = loadImage;
+			_src = new AttributeMappedValue<string>(this, "src");
+			_alt = new AttributeMappedValue<string>(this, "alt");
+			_useMap = new AttributeMappedValue<string>(this, "usemap");
+			_isMap = new AttributeMappedBoolValue(this, "ismap");
+		}
+
+		/// <summary>
+		/// Reflects 'alt' attribute value.
+		/// </summary>
+		public string Alt
+		{
+			get => _alt.Value;
+			set => _alt.Value = value;
+		}
+
+		/// <summary>
+		/// Reflects 'usemap' attribute value that if present, can indicate that the image has an associated.
+		/// </summary>
+		public string UseMap
+		{
+			get => _useMap.Value;
+			set => _useMap.Value = value;
+		}
+
+		/// <summary>
+		/// Reflects 'ismap' attribute value.
+		/// </summary>
+		public bool IsMap
+		{
+			get => _isMap.Value;
+			set => _isMap.Value = value;
+		}
+
+		/// <summary>
+		/// Is a String that reflects the src HTML attribute, containing the full URL of the image including base URI.
+		/// </summary>
+		public string Src
+		{
+			get => _src.Value;
+			set
+			{
+				_src.Value = value;
+
+				LoadImage(value);
+			}
+		}
+
+		private async Task LoadImage(string value)
+		{
+			var wasError = false;
+
+			try
+			{
+				_loadedImage = await _loadImage(value);
+				if (_loadedImage == null)
+				{
+					var errorEvent = (ErrorEvent) OwnerDocument.CreateEvent("errorevent");
+					errorEvent.InitEvent("error", true, true);
+					errorEvent.ErrorEventInit("", "", 0, 0, ""); //todo: fill error
+					DispatchEvent(errorEvent);
+					wasError = true;
+				}
+			}
+			catch (Exception ex)
+			{
+				var errorEvent = (ErrorEvent) OwnerDocument.CreateEvent("errorevent");
+				errorEvent.InitEvent("error", true, true);
+				errorEvent.ErrorEventInit(ex.Message, "", 0, 0, ""); //todo: fill error
+				DispatchEvent(errorEvent);
+				wasError = true;
+			}
+
+			if (!wasError)
+			{
+				var loadEvent = OwnerDocument.CreateEvent("Event");
+				loadEvent.InitEvent("load", true, true);
+				DispatchEvent(loadEvent);
+			}
+		}
+
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public int NaturalWidth => _loadedImage != null ? _loadedImage.Width : 0;
+		public int NaturalHeight => _loadedImage != null ? _loadedImage.Height : 0;
+		public bool Completed => string.IsNullOrEmpty(Src) || _loadedImage != null;
+		
+		/// <summary>
+		/// Fired immediately after an element has been loaded.
+		/// </summary>
+		public event Action<Event> OnLoad;
+		public event Action<Event> OnError;
+		
+		protected override void CallDirectEventSubscribers(Event evt)
+		{
+			base.CallDirectEventSubscribers(evt);
+
+			switch (evt.Type)
+			{
+				case "load":Handle("onload", OnLoad, evt);break;
+				case "error":Handle("onerror", OnError, evt);break;
+			}
+		}
+	}
+}
