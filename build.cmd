1887be975ce80aa31d6b070accefce5d3993f268
knyaz <petuhov_k@mail.ru>
2019-11-23 22:45:56 +0700

    Scripting.Jurassic project has been moved to its own repository.

--- build.cmd
+++ build.cmd
@@ -4,8 +4,4 @@ dotnet build -c Release source/Knyaz.Optimus/Knyaz.Optimus.csproj /p:VersionSuff
 
 dotnet build -c Release source/Knyaz.Optimus.Tests/Knyaz.Optimus.Tests.csproj /p:VersionSuffix=%suff%
 
-dotnet build -c Release source/Knyaz.Optimus.Scripting.Jurassic/Knyaz.Optimus.Scripting.Jurassic.csproj /p:VersionSuffix=%suff%
-
 dotnet pack -c Release source/Knyaz.Optimus/Knyaz.Optimus.csproj /p:VersionSuffix=%suff% --no-build
-
-dotnet pack -c Release source/Knyaz.Optimus.Scripting.Jurassic/Knyaz.Optimus.Scripting.Jurassic.csproj /p:VersionSuffix=%suff% --no-build
