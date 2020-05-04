ea54c549bf5657ac71181c0377163e8e7926eb5c
knyaz <petuhov_k@mail.ru>
2020-05-04 18:19:24 +0700

    Minor tests refactoring.

--- source/Knyaz.Optimus.Tests/EngineTests/EngineUnitTests.ConcurentExecution.cs
+++ source/Knyaz.Optimus.Tests/EngineTests/EngineUnitTests.ConcurentExecution.cs
@@ -15,7 +15,7 @@ namespace Knyaz.Optimus.Tests.EngineTests
 				.Resource("http://localhost/script.js", "document.write('<script src=\"script2.js\"></script>');")
 				.Resource("http://localhost/script2.js","document.write('<div id=d2>ABC</div>');");
 
-			var task = Task.Run(() => resourceProvider.OpenPage("http://localhost"));
+			var task = Task.Run(() => TestingEngine.BuildJint(resourceProvider).OpenUrl("http://localhost"));
 			Assert.IsTrue(task.Wait(1000));
 			var page = task.Result;
 			System.Diagnostics.Debug.Write(page.Document.InnerHTML);
